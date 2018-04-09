7bd7dbfb89c80715db1ada08bf7cb69482acda5d
DarthHamza <hamzamakia@gmail.com>
2018-04-09 14:05:10 +0300

    Complete solution

--- restaurants/views.py
+++ restaurants/views.py
@@ -4,16 +4,30 @@ def welcome(request):
     return render(request, 'index.html', {'msg':'Hello World!'})
 
 def restaurant_list(request):
-
     context = {
-
+        "my_list":[
+            {
+                'name':'Subway',
+                'food_type':'Sandwiches.',
+            },
+            {
+                'name':'Steers',
+                'food_type':'Burgers, Fries, and Pasta.',
+            },
+            {
+                'name':'Backyard',
+                'food_type':'Been there, done that...',
+            },
+        ]
     }
     return render(request, 'list.html', context)
 
 
 def restaurant_detail(request):
-
     context = {
-
+        "my_object":{
+                        'name':'Subway',
+                        'food_type':'Sandwiches.',
+                    },
     }
     return render(request, 'detail.html', context)
