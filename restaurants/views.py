e5df14025da42f33266be8372a4db22708045f3d
Nabil Fares <nabilfares@Nabils-MacBook-Pro.local>
2019-01-22 20:43:39 +0300

    task03

--- restaurants/views.py
+++ restaurants/views.py
@@ -4,16 +4,23 @@ def welcome(request):
     return render(request, 'index.html', {'msg':'Hello World!'})
 
 def restaurant_list(request):
-
     context = {
-
+        'my_list' : [
+            { 'restaurant_name' : 'Zeinayus',
+              'food_type' : 'Sea Food'
+            },
+            {'restaurant_name' : 'resto',
+             'food_type' : 'vegi Food'
+            }
+        ]
     }
     return render(request, 'list.html', context)
 
 
 def restaurant_detail(request):
-
-    context = {
-
+    context = { 'my_object' : {
+    		'restaurant_name' : 'Zeinayus',
+            'food_type' : 'Sea Food'
+    		}
     }
     return render(request, 'detail.html', context)
